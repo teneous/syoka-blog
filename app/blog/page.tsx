7d814c07bde5a784b15d93eec7d80aa4857b681b
xiaokai <xiaokai@tengyun.com>
2025-05-14 21:55:00 +0800

    #fix 1) add weread

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
-import ListLayout from '@/layouts/ListLayoutWithTags'
+import ListLayout from '@/components/layouts/ListLayoutWithTags'
 
 const POSTS_PER_PAGE = 5
 
